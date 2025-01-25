9715fad66dcefb9cd40f62999b01798453dfe256
WaveringAna <ana@nekomimi.pet>
2025-01-24 22:53:06 -0500

    yolo commit

--- src/backend.ts
+++ src/backend.ts
@@ -2,25 +2,30 @@ import index from "./public/index.html";
 import { serve } from "bun";
 
 serve({
-	static: {
-		"/": index,
-		"/favicon.ico": new Response(await Bun.file("./src/public/avatar.jpg").bytes(), {
-			headers: {
-				"Content-Type": "image/x-icon",
-			},
-		}),
-		"/avatar.jpg": new Response(await Bun.file("./src/public/avatar.jpg").bytes(), {
-			headers: {
-				"Content-Type": "image/jpeg",
-			},
-		}),
-	},
+    static: {
+        "/": index,
+        "/output.css": new Response(await Bun.file("./src/public/output.css").bytes(), {
+            headers: {
+                "Content-Type": "text/css",
+            },
+        }),
+        "/favicon.ico": new Response(await Bun.file("./src/public/avatar.jpg").bytes(), {
+            headers: {
+                "Content-Type": "image/x-icon",
+            },
+        }),
+        "/avatar.jpg": new Response(await Bun.file("./src/public/avatar.jpg").bytes(), {
+            headers: {
+                "Content-Type": "image/jpeg",
+            },
+        }),
+    },
 
-	development: true,
+    development: true,
 
-	async fetch(req) {
-		return new Response("hello world");
-	},
+    async fetch(req) {
+        return new Response("hello world");
+    },
 });
 
 console.log("Server started at http://localhost:3000");
