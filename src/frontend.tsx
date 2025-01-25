2eff2aa38124536faf00d43ee3cd2c01ce61a27f
WaveringAna <ana@nekomimi.pet>
2025-01-24 22:12:03 -0500

    yolo commit

--- src/frontend.tsx
+++ src/frontend.tsx
@@ -3,6 +3,8 @@ import { createRoot } from "react-dom/client";
 import Home from "./pages/index.tsx";
 
 document.addEventListener("DOMContentLoaded", () => {
-    const root = createRoot(document.getElementById("root"));
+    const rootElement = document.getElementById("root");
+    if (!rootElement) throw new Error("Root element not found");
+    const root = createRoot(rootElement);
     root.render(<Home />);
 });
